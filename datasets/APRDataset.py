ec3d8fbfc6c6e3cc533c131ff0ad5502ec844a50
Srinivas Ravuri <srinivas.ravuri@dai-labor.de>
2023-12-13 08:08:02 +0100

    feat: add load lidar data
    Signed-off-by: Srinivas Ravuri <srinivas.ravuri@dai-labor.de>

--- datasets/APRDataset.py
+++ datasets/APRDataset.py
@@ -21,6 +21,7 @@ class APRDataset(Dataset):
         self.min_pose = []
         self.max_pose = []
         self.cam_type = cfg['cam_type']
+        self.modality = cfg['modality']
         self.get_min_max_pose()
         if mode == 'train':
             self.metadata = self.metadata[:int(len(self.metadata) * (1 - self.val_split))]
@@ -58,16 +59,39 @@ class APRDataset(Dataset):
     
     def get_image(self, img_file):
         img = imread(img_file)
-        # img = cv2.imread(img_file)
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         return img
-
+    def lidar_to_histogram_features(self,lidar):
+        """
+        Convert LiDAR point cloud into 2-bin histogram over 256x256 grid
+        """
+        def splat_points(point_cloud):
+            # 256 x 256 grid
+            pixels_per_meter = 8
+            hist_max_per_pixel = 5
+            x_meters_max = 30
+            x_meters_min = -30
+            y_meters_max = 40
+            y_meters_min = -10
+            xbins = np.linspace(x_meters_min, x_meters_max, 32*pixels_per_meter+1)
+            ybins = np.linspace(y_meters_min, y_meters_max, 32*pixels_per_meter+1)
+            hist = np.histogramdd(point_cloud[..., :2], bins=(xbins, ybins))[0]
+            hist[hist>hist_max_per_pixel] = hist_max_per_pixel
+            overhead_splat = hist/hist_max_per_pixel
+            return overhead_splat
+        below = lidar[lidar[...,2]<=2.0]
+        above = lidar[lidar[...,2]>2.0]
+        below_features = splat_points(below)
+        above_features = splat_points(above)
+        features = np.stack([above_features, below_features], axis=-1).astype(np.float32)
+        # features = np.transpose(features, (2, 0, 1))
+        return features
+    
     def load_metadata(self):
         # load poses.txt file as metadata
         if self.mode == "val":
             self.root_dir = os.path.join(self.cfg['root_dir'], "train")
         with open(os.path.join(self.root_dir, 'poses.txt')) as f:
-            metadata = pd.read_csv(f, sep=' ', header=None, names=['image1','image2','image3','x', 'y', 'z', 'qw', 'qx', 'qy', 'qz'])
+            metadata = pd.read_csv(f, sep=' ', header=None, names=['image1','image2','image3','lidar','x', 'y', 'z', 'qw', 'qx', 'qy', 'qz'])
             metadata = metadata.dropna() # drop NaN rows
             metadata = metadata.iloc[1:] # drop first row as it's a duplicate of column names           
         return metadata
@@ -79,33 +103,46 @@ class APRDataset(Dataset):
         row = self.metadata.iloc[index]
         qpose = self.get_pose([row['x'], row['y'], row['z']])
         qrot =  self.get_pose([row['qw'], row['qx'], row['qy'], row['qz']])
-        for i in self.cam_type:
-            if i == 'camera1': # camera_front_wide
-                img = self.get_image(os.path.join(self.root_dir, i , row['image1'])) 
-                qpose = self.get_pose(qpose) 
-                qrot = self.get_pose(qrot)
-            elif i =='camera2': # camera_front_right
-                img = self.get_image(os.path.join(self.root_dir, i , row['image2'])) 
-                qpose = self.get_pose(qpose) 
-                qrot = self.get_pose(qrot)
-                qrot = self.rotate_quaternion(qrot,math.radians(-60)) 
-            elif i == 'camera3': # camera_front_left
-                img = self.get_image(os.path.join(self.root_dir, i , row['image3'])) 
-                qpose = self.get_pose(qpose) 
-                qrot = self.get_pose(qrot)
-                qrot = self.rotate_quaternion(qrot,math.radians(60)) 
-            
+        if self.modality == 'image':
+            for i in self.cam_type:
+                if i == 'camera1': # camera_front_wide
+                    img = self.get_image(os.path.join(self.root_dir, i , row['image1'])) 
+                    qpose = self.get_pose(qpose) 
+                    qrot = self.get_pose(qrot)
+                elif i =='camera2': # camera_front_right
+                    img = self.get_image(os.path.join(self.root_dir, i , row['image2'])) 
+                    qpose = self.get_pose(qpose) 
+                    qrot = self.get_pose(qrot)
+                    qrot = self.rotate_quaternion(qrot,math.radians(-60)) 
+                elif i == 'camera3': # camera_front_left
+                    img = self.get_image(os.path.join(self.root_dir, i , row['image3'])) 
+                    qpose = self.get_pose(qpose) 
+                    qrot = self.get_pose(qrot)
+                    qrot = self.rotate_quaternion(qrot,math.radians(60)) 
+                if self.transforms is not None:
+                    img = self.transforms(img)
+                npose = self.normalize_pose(qpose, self.min_pose, self.max_pose) # normalize pose
+                pose = np.concatenate((npose, qrot), axis=0) # concatenate pose and rotation
+                sample = {
+                    'img': img, 
+                    'pose': torch.tensor(pose).float()}
+                return sample
+        elif self.modality == 'lidar':
+            points = np.fromfile(os.path.join(self.root_dir, 'lidar', row['lidar']), dtype=np.float32).reshape(-1, 4)
+            points_histogram = self.lidar_to_histogram_features(points)
             if self.transforms is not None:
-                img = self.transforms(img)
-
+                points_histogram = self.transforms(points_histogram)
+            qpose = self.get_pose(qpose) 
+            qrot = self.get_pose(qrot)
             npose = self.normalize_pose(qpose, self.min_pose, self.max_pose) # normalize pose
             pose = np.concatenate((npose, qrot), axis=0) # concatenate pose and rotation
-            
             sample = {
-                'img': img, 
-                'pose': torch.tensor(pose).float()}
-            
+                    'img': points_histogram, 
+                    'pose': torch.tensor(pose).float()}
             return sample
+        else:
+            pass
+
 
     def normalize_pose(self,pose, min_pose, max_pose):
         if len(min_pose) == 0:
